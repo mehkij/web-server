45de94b38a9ae63d093eb30132e8b36f226b37c5
mehkij <disismehki@gmail.com>
2024-12-08 12:00:14 -0500

    Serve static files

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import "net/http"
 
 func main() {
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: serveMux,
+		Handler: mux,
 	}
 
 	err := server.ListenAndServe()
