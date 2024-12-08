f1294effae860339e2ecff029356038e34c017e8
mehkij <disismehki@gmail.com>
2024-12-08 11:50:00 -0500

    Use NewServeMux

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import "net/http"
 
 func main() {
-	serveMux := http.ServeMux{}
+	serveMux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: &serveMux,
+		Handler: serveMux,
 	}
 
 	err := server.ListenAndServe()
