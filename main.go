0a0a2a6e34803c3ae080849edac4b747aaa90c40
mehkij <disismehki@gmail.com>
2024-12-09 08:08:02 -0500

    Extract handler function for readiness endpoint

--- main.go
+++ main.go
@@ -8,16 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
-
-		_, err := w.Write([]byte("OK"))
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -26,4 +17,10 @@ func main() {
 
 	log.Println("Serving files from . on port: 8080")
 	log.Fatal(server.ListenAndServe())
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
