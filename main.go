16b8756d4f479b3060791860736b722d330eafb9
mehkij <disismehki@gmail.com>
2024-12-09 09:41:48 -0500

    Create middleware

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	var apiCfg apiConfig
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server{
