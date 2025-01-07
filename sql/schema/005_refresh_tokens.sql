5b705a06e1e11b1c5321f3eb58c556be4dcb1bab
mehkij <disismehki@gmail.com>
2025-01-06 21:19:17 -0500

    Fix table schema: refresh_tokens

--- sql/schema/005_refresh_tokens.sql
+++ sql/schema/005_refresh_tokens.sql
@@ -2,6 +2,7 @@
 CREATE TABLE refresh_tokens(
     token TEXT PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     revoked_at TIMESTAMP,
     user_id UUID,
