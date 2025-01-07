3c2ae23f049c7e25b6f6ad9f0c648936824ec97e
mehkij <disismehki@gmail.com>
2025-01-06 19:23:59 -0500

    Create new migration: refresh_tokens

---
+++ sql/schema/005_refresh_tokens.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE refresh_tokens(
+    token TEXT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    revoked_at TIMESTAMP,
+    user_id UUID,
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE refresh_tokens;
